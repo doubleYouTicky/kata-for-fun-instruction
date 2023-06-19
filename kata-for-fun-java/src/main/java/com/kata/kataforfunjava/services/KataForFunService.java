7106afe0d465001f25a891eceed535acd040a883
william <william.ticky@outlook.fr>
2023-06-19 22:45:56 +0200

    [feature] refactoring

--- kata-for-fun-java/src/main/java/com/kata/kataforfunjava/services/KataForFunService.java
+++ kata-for-fun-java/src/main/java/com/kata/kataforfunjava/services/KataForFunService.java
@@ -3,29 +3,42 @@ package com.kata.kataforfunjava.services;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Service
 public class KataForFunService {
-    public String convertNumber( int inputNumber) {
+
+    public static final String KATA = "Kata";
+    public static final String FOR = "For";
+    public static final String FUN = "Fun";
+
+    public String convertNumber(int inputNumber) {
         StringBuilder result = new StringBuilder();
-        if (inputNumber % 3 == 0) {
-            result.append("Kata");
-        }
-        if (inputNumber % 5 == 0) {
-            result.append("For");
-        }
+
+        result.append(convertDivisibleInput(inputNumber, 3, KATA));
+        result.append(convertDivisibleInput(inputNumber, 5, FOR));
+
         String intputAsString = String.valueOf(inputNumber);
         for (char c: intputAsString.toCharArray()) {
-            if ('3' == c) {
-                result.append("Kata");
-            }
-            if ('5' == c) {
-                result.append("For");
-            }
-            if ('7' == c) {
-                result.append("Fun");
-            }
-
+            result.append(convertFoundNumber(c));
         }
         return result.isEmpty() ? intputAsString : result.toString();
+    }
+
+    private String convertFoundNumber(char number) {
+        return switch (number) {
+            case '3' -> KATA;
+            case '5' -> FOR;
+            case '7' -> FUN;
+            default -> "";
+        };
+    }
+
+    private String convertDivisibleInput(int inputNumber, int divider, String pushedString) {
+        if(inputNumber % divider == 0) {
+            return pushedString;
+        }
+        return Strings.EMPTY;
     }
 }
