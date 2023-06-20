acaf66ddd713930b8e7c1a8764bc43f0a47c43ac
william <william.ticky@outlook.fr>
2023-06-20 10:13:45 +0200

    [refactoring] using private static fields

--- kata-for-fun-java/src/main/java/com/kata/kataforfunjava/services/KataForFunService.java
+++ kata-for-fun-java/src/main/java/com/kata/kataforfunjava/services/KataForFunService.java
@@ -3,15 +3,12 @@ package com.kata.kataforfunjava.services;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Service
 public class KataForFunService {
 
-    public static final String KATA = "Kata";
-    public static final String FOR = "For";
-    public static final String FUN = "Fun";
+    private static final String KATA = "Kata";
+    private static final String FOR = "For";
+    private static final String FUN = "Fun";
 
     public String convertNumber(int inputNumber) {
         StringBuilder result = new StringBuilder();
